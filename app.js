1c67d4e1fa7be100207e03befd1753aa9b1c1aea
mo4rahman <mo4rahman@gmail.com>
2022-04-06 15:31:28 -0400

    Add a few pseudo-code comments to break down this project

---
+++ app.js
@@ -0,0 +1,31 @@
+// Some pesudo-code to get a general sense of what we need to do to get at least a functional and working mvp
+
+// We will want to do majority of the work in JS first, and make sure we have the
+// functionality down before we dive into CSS and HTML. However, we will start 
+// with some basic CSS and HTML to at least visualize our game.
+
+
+// Create Game board:
+// Create a square board in the center of the page, with lines that divide the 
+// board into a 4x4 grid of empty white space
+
+// Create Tiles:
+// Have each square in our game board grid have the ability to display a square
+// tile for each square (we can change the color and number based on js events)
+
+// Initializing game:
+// Have 2 random squares in our grid be a tile of 2 with a color (all same 
+// numbers should have the same color. every 2 will be beige, every 4 will be a 
+// darker beige, etc).
+//
+
+// Events:
+// Make JS listen for arrow key presses:
+// (have to research, but I think there should be an option for a 'keydown' press
+// or something like that)
+// Also create arrow key buttons that when clicked, will have the same 
+// functionality as when pressing respective arrow keys.
+// Pressing a direction should have the tiles all shift the most they can in that
+// direction (preferably with a sliding animation instead of just reappearing 
+// where they should be and refresshing.)
+//
