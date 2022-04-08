60cf665cb4525278ff20fa1ff8581913987dc357
mo4rahman <mo4rahman@gmail.com>
2022-04-08 17:16:20 -0400

    Late afternoon commit - organizing the code to have all of my initial pesudo code at the top of the file instead of trying to work on every piece as I code. I will refer to it on the top.

--- app.js
+++ app.js
@@ -19,6 +19,39 @@
 // numbers should have the same color. every 2 will be beige, every 4 will be a
 // darker beige, etc).
 
+// Events:
+// Make JS listen for arrow key presses:
+// (have to research, but I think there should be an option for a 'keydown' press
+// or something like that. Each key should have a number attached to it.)
+// Also create arrow key buttons that when clicked, will have the same
+// functionality as when pressing respective arrow keys.
+// Pressing a direction should have the tiles all shift the most they can in that
+// direction (preferably with a sliding animation instead of just reappearing
+// where they should be and refreshing.).
+// The slide functionality will probably be put into a slide function since we
+// will be doing that frequently.
+// We will probably use 4 arrays and for loops to check each row when sliding in
+// a certain direction.
+
+// Checking Conditions:
+// When moving in a direction, check to see if the preceding array (assuming
+// there is, we can have a check for how far the element can travel) has any
+// numbers (0 or empty string for blank). Also check to see if the number is same,
+// if so then add the number and make the previous array element blank.
+// Have a gameRunning flag to be true unless the board is filled with nowhere to
+// slide, then that means the player has lost the game.
+
+// Extra buttons/displays
+// Have a New Game button to reset the game board (possibly make a
+// "createGameboard()" function and have all that in there.)
+// Have a score board keeping track of score, as well as comparing the score
+// once the player loses and replaces best score if it's better than the best
+// score, else does not save the score.
+
+// Stretch Goals
+// If i finish the project early, I can dive into creating a light mode/dark mode
+// toggle to change the colors of the page/board.
+
 // We'll have 2 arrays. One with all the stored tiles, and one empty array that
 // we will fill and update as the game is being played.
 const allGameTiles = document.querySelectorAll(".game-tile");
@@ -69,7 +102,9 @@ function shiftNumbersLeft() {
   console.log("shift left");
 }
 function checkKey(e) {
-  //   e = e || window.event;
+  // Used just in case somehow, someone is running my game in an earlier
+  // version of Internet Explorer that doesn't pass in the window event.
+  e = e || window.event;
 
   if (e.keyCode === 38) {
     // up arrow
@@ -86,35 +121,3 @@ function checkKey(e) {
   }
 }
 document.onkeydown = checkKey;
-// Events:
-// Make JS listen for arrow key presses:
-// (have to research, but I think there should be an option for a 'keydown' press
-// or something like that. Each key should have a number attached to it.)
-// Also create arrow key buttons that when clicked, will have the same
-// functionality as when pressing respective arrow keys.
-// Pressing a direction should have the tiles all shift the most they can in that
-// direction (preferably with a sliding animation instead of just reappearing
-// where they should be and refreshing.).
-// The slide functionality will probably be put into a slide function since we
-// will be doing that frequently.
-// We will probably use 4 arrays and for loops to check each row when sliding in
-// a certain direction.
-
-// Checking Conditions:
-// When moving in a direction, check to see if the preceding array (assuming
-// there is, we can have a check for how far the element can travel) has any
-// numbers (0 or empty string for blank). Also check to see if the number is same,
-// if so then add the number and make the previous array element blank.
-// Have a gameRunning flag to be true unless the board is filled with nowhere to
-// slide, then that means the player has lost the game.
-
-// Extra buttons/displays
-// Have a New Game button to reset the game board (possibly make a
-// "createGameboard()" function and have all that in there.)
-// Have a score board keeping track of score, as well as comparing the score
-// once the player loses and replaces best score if it's better than the best
-// score, else does not save the score.
-
-// Stretch Goals
-// If i finish the project early, I can dive into creating a light mode/dark mode
-// toggle to change the colors of the page/board.
